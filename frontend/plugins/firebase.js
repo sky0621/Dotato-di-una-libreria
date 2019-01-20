9ca22e06ccd929fce07a81c0bbc093185cf3cfc8
sky0621 <sky14work@gmail.com>
2019-01-20 20:08:44 +0900

    third

---
+++ frontend/plugins/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(
+    {
+      apiKey: process.env.apiKey,
+      authDomain: process.env.authDomain,
+      databaseURL: process.env.databaseURL,
+      projectId: process.env.projectId,
+      storageBucket: process.env.storageBucket,
+      messagingSenderId: process.env.messagingSenderId
+    }
+  );
+}
+
+export default firebase
